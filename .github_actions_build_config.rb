4cac2d23ed379546a1e84bd18ad8dadb8e097f88
Hiroki Mori <yamori813@yahoo.co.jp>
2022-02-25 16:56:59 +0900

    add my mrbgem

--- .github_actions_build_config.rb
+++ .github_actions_build_config.rb
@@ -2,6 +2,7 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
   conf.gem File.expand_path(File.dirname(__FILE__))
+  conf.gem :github => 'yamori813/mruby-cm6206'
   conf.enable_test
 
   if ENV['DEBUG'] == 'true'
