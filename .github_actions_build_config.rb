2dd54a129b3172873c4d5c3cae8020a5cc2e5fd1
Hiroki Mori <yamori813@yahoo.co.jp>
2022-02-25 17:00:16 +0900

    revart

--- .github_actions_build_config.rb
+++ .github_actions_build_config.rb
@@ -2,7 +2,6 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
   conf.gem File.expand_path(File.dirname(__FILE__))
-  conf.gem :github => 'yamori813/mruby-cm6206'
   conf.enable_test
 
   if ENV['DEBUG'] == 'true'
